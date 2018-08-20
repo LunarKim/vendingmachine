be0abdc7f25653a8a4bad3efaaecf6d5b20ad76d
MoonheeKim <you@example.com>
2018-08-20 21:59:52 +0900

    첫번째커밋

--- test_vm.py
+++ test_vm.py
@@ -1,20 +1,17 @@
-from run import run, init
-
+from vm import run, init
 
 def test_initial_change_should_be_zero():
     init()
-    assert "잔액은 0원 입니다" == run("잔액")
+    assert "잔액은 0원입니다" == run("잔액")
 
-
-def test_insert_coin_and_check():
+def test_check_coin():
     init()
     assert "100원을 넣었습니다" == run("동전 100")
-    assert "잔액은 100원 입니다" == run("잔액")
 
-
-
-def test_accumulateion_of_change():
+def test_accumulation_of_change():
     init()
     run("동전 100")
     run("동전 100")
-    assert "잔액은 200원 입니다" == run("잔액")
+    assert "잔액은 200원입니다" == run("잔액")
+
+    
--- vm.py
+++ vm.py
@@ -4,16 +4,18 @@ def init():
     global change
     change = 0
 
+
+
+
 def run(raw):
     global change
 
     tokens = raw.split(" ")
-    cmd = tokens[0]
     cmd, params = tokens[0], tokens[1:]
 
     if cmd == "잔액":
-        return "잔액은 " + str(change) + "원 입니다"
+        return "잔액은 " + str(change) + "원입니다"
     else:
         coin = params[0]
         change += int(coin)
-        return coin +"원을 넣었습니다"
+        return coin + "원을 넣었습니다"
